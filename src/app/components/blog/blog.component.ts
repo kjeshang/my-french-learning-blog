4d452b138aaf6a2c06faa00f7b4db89517abc039
kjeshang <kunal@zypgallery.ca>
2024-11-27 01:01:06 -0800

    Created baseline functionality for blog component

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,11 +1,70 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { MarkdownModule } from 'ngx-markdown';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+
+export interface BlogData {
+  id: number;
+  name: string;
+  level: string;
+  reference: string;
+  date: string;
+  post: string;
+}
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [CommonModule, MarkdownModule, MatGridListModule, MatFormFieldModule, MatTableModule, MatCardModule, MatChipsModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.scss'
 })
-export class BlogComponent {
+export class BlogComponent implements OnInit {
+
+  json_link: string = 'https://raw.githubusercontent.com/kjeshang/MyFrenchLearningBlog_Backend/refs/heads/main/data.json';
+
+  displayedColumns: string[] = [
+    // 'id', 
+    'name', 
+    'level', 
+    'reference', 
+    'date', 
+    // 'post'
+  ];
+
+  dataSource: BlogData[] = [];
+
+  selectedCell: any = null;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  fetchData(): void {
+    const url = this.json_link;
+    this.http.get<BlogData[]>(url).subscribe((data) => {
+      this.dataSource = data;
+    });
+  }
+
+  onRowClick(row: any): void {
+    this.selectedCell = row;
+  }
+
+  showBlogPostList = true;
+  toggleBlogPostList_options = {
+    "hide":"Masquer les articles de blog",
+    "show":"Afficher les articles de blog"
+  };
+
+  toggleBlogListContent() {
+    this.showBlogPostList = !this.showBlogPostList;
+  }  
 
 }
